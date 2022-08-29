a06efe0d717fcecc9bc0f2f28b7548ec3b42914d
bufx <bufanxiang@gmail.com>
2022-08-29 15:00:39 +0800

    Modify the RestTemplate plug-in to support reflection

--- apm-sniffer/apm-sdk-plugin/spring-plugins/resttemplate-4.x-plugin/src/main/java/org/apache/skywalking/apm/plugin/spring/resttemplate/sync/HttpEntityRequestCallbackInterceptor.java
+++ apm-sniffer/apm-sdk-plugin/spring-plugins/resttemplate-4.x-plugin/src/main/java/org/apache/skywalking/apm/plugin/spring/resttemplate/sync/HttpEntityRequestCallbackInterceptor.java
@@ -18,27 +18,30 @@
 
 package org.apache.skywalking.apm.plugin.spring.resttemplate.sync;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import java.io.ByteArrayOutputStream;
-import java.lang.reflect.Method;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
+import org.apache.skywalking.apm.agent.core.base64.Base64;
+import org.apache.skywalking.apm.agent.core.conf.Config;
 import org.apache.skywalking.apm.agent.core.context.CarrierItem;
 import org.apache.skywalking.apm.agent.core.context.ContextCarrier;
-import org.apache.skywalking.apm.agent.core.context.esb.EsbBody;
+import org.apache.skywalking.apm.agent.core.logging.api.ILog;
+import org.apache.skywalking.apm.agent.core.logging.api.LogManager;
 import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.EnhancedInstance;
 import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.InstanceMethodsAroundInterceptor;
 import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.MethodInterceptResult;
 import org.apache.skywalking.apm.plugin.spring.resttemplate.helper.RestTemplateRuntimeContextHelper;
+import org.apache.skywalking.apm.util.StringUtil;
 import org.springframework.http.client.AbstractClientHttpRequest;
 import org.springframework.http.client.ClientHttpRequest;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * {@link HttpEntityRequestCallbackInterceptor} intercept the TestTemplate$HttpEntityRequestCallback args resolve ;
  */
 public class HttpEntityRequestCallbackInterceptor implements InstanceMethodsAroundInterceptor {
+
+    private static final ILog LOGGER = LogManager.getLogger(HttpEntityRequestCallbackInterceptor.class);
 
     @Override
     public void beforeMethod(EnhancedInstance objInst, Method method, Object[] allArguments, Class<?>[] argumentsTypes, MethodInterceptResult result) throws Throwable {
@@ -49,18 +52,8 @@ public class HttpEntityRequestCallbackInterceptor implements InstanceMethodsArou
     public Object afterMethod(EnhancedInstance objInst, Method method, Object[] allArguments, Class<?>[] argumentsTypes, Object ret) throws Throwable {
         ClientHttpRequest httpRequest = (ClientHttpRequest) allArguments[0];
         AbstractClientHttpRequest clientHttpRequest = (AbstractClientHttpRequest) httpRequest;
-        ByteArrayOutputStream byteArrayOutputStream = (ByteArrayOutputStream) clientHttpRequest.getBody();
-        String body = byteArrayOutputStream.toString();
-        String charset;
         try {
-            charset = httpRequest.getHeaders().getContentType().getParameter("charset");
-        } catch (Exception e) {
-            charset = StandardCharsets.UTF_8.name();
-        }
-        try {
-            Gson gson = new GsonBuilder().serializeNulls().create();
-            EsbBody esbBody = gson.fromJson(body, EsbBody.class);
-            if (esbBody.hasAgentHeader()) {
+            if (StringUtil.isNotEmpty(Config.Agent.ESB_TRACE_CLASS_PATH)) {
                 Map<String, String> agentHeaderMap = new HashMap<>();
                 ContextCarrier contextCarrier = RestTemplateRuntimeContextHelper.getContextCarrier();
                 CarrierItem next = contextCarrier.items();
@@ -68,11 +61,13 @@ public class HttpEntityRequestCallbackInterceptor implements InstanceMethodsArou
                     next = next.next();
                     agentHeaderMap.put(next.getHeadKey(), next.getHeadValue());
                 }
-                byteArrayOutputStream.reset();
-                byteArrayOutputStream.write(esbBody.updateTraceContext(body, new Gson().toJson(agentHeaderMap))
-                    .getBytes(charset));
+                Class aClass = Class.forName(Config.Agent.ESB_TRACE_CLASS_PATH);
+                Object aObject = aClass.newInstance();
+                Method invokeMethod = findInvokeMethod(aObject);
+                invokeMethod.invoke(aObject, clientHttpRequest, Base64.encode(agentHeaderMap.toString()));
             }
         } catch (Exception e) {
+            LOGGER.info("RestTemplate processing reflection method error:. Exception:{}", e);
             return ret;
         }
         return ret;
@@ -81,5 +76,20 @@ public class HttpEntityRequestCallbackInterceptor implements InstanceMethodsArou
     @Override
     public void handleMethodException(EnhancedInstance objInst, Method method, Object[] allArguments, Class<?>[] argumentsTypes, Throwable t) {
 
+    }
+
+    public Method findInvokeMethod(Object object) {
+        Method invokeMethod = null;
+        Method[] methods = object.getClass().getDeclaredMethods();
+        if (methods != null && methods.length > 0) {
+            for (Method m : methods) {
+                Class<?>[] clazzs = m.getParameterTypes();
+                if (m.getName().equals(Config.Agent.ESB_TRACE_CLASS_SEND_METHOD) && clazzs.length == 2) {
+                    invokeMethod = m;
+                    break;
+                }
+            }
+        }
+        return invokeMethod;
     }
 }
