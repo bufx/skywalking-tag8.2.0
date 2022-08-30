3146db5adff8fc5a6ff9eeebfdc4a9cc7d774748
bufx <bufanxiang@gmail.com>
2022-08-30 10:33:23 +0800

    The union-Paygate agent plug-in is completed

--- apm-sniffer/apm-sdk-plugin/union-paygate-plugin/src/main/java/org/apache/skywalking/apm/plugin/unionpaygate/CoreControllerInterceptor.java
+++ apm-sniffer/apm-sdk-plugin/union-paygate-plugin/src/main/java/org/apache/skywalking/apm/plugin/unionpaygate/CoreControllerInterceptor.java
@@ -18,24 +18,33 @@
 
 package org.apache.skywalking.apm.plugin.unionpaygate;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
-import javax.servlet.http.HttpServletRequest;
-
+import org.apache.skywalking.apm.agent.core.base64.Base64;
+import org.apache.skywalking.apm.agent.core.conf.Config;
+import org.apache.skywalking.apm.agent.core.context.CarrierItem;
 import org.apache.skywalking.apm.agent.core.context.ContextCarrier;
 import org.apache.skywalking.apm.agent.core.context.ContextManager;
+import org.apache.skywalking.apm.agent.core.context.SW8CarrierItem;
 import org.apache.skywalking.apm.agent.core.context.tag.Tags;
 import org.apache.skywalking.apm.agent.core.context.tag.Tags.HTTP;
 import org.apache.skywalking.apm.agent.core.context.trace.AbstractSpan;
 import org.apache.skywalking.apm.agent.core.context.trace.SpanLayer;
+import org.apache.skywalking.apm.agent.core.logging.api.ILog;
+import org.apache.skywalking.apm.agent.core.logging.api.LogManager;
 import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.EnhancedInstance;
 import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.InstanceMethodsAroundInterceptor;
 import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.MethodInterceptResult;
 import org.apache.skywalking.apm.network.trace.component.ComponentsDefine;
+import org.apache.skywalking.apm.util.StringUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CoreControllerInterceptor implements InstanceMethodsAroundInterceptor {
+
+    private static final ILog LOGGER = LogManager.getLogger(CoreControllerInterceptor.class);
 
     @Override
     public void beforeMethod(EnhancedInstance objInst, Method method, Object[] allArguments, Class<?>[] argumentsTypes,
@@ -46,16 +55,46 @@ public class CoreControllerInterceptor implements InstanceMethodsAroundIntercept
         requestFiled.setAccessible(true);
         HttpServletRequest request = (HttpServletRequest) requestFiled.get(object);
 
-        Field dataFiled = object.getClass().getSuperclass().getDeclaredField("data");
-        dataFiled.setAccessible(true);
-        Map dataMap = (HashMap) dataFiled.get(object);
+        boolean hasAgentHeader = false;
+        Map<String, String> agentHeaderMap = new HashMap<>();
+
+        if (StringUtil.isEmpty(request.getHeader(SW8CarrierItem.HEADER_NAME))) {
+            try {
+                if (StringUtil.isNotEmpty(Config.Agent.ESB_TRACE_CLASS_PATH)) {
+                    Class aClass = Class.forName(Config.Agent.ESB_TRACE_CLASS_PATH);
+                    Object aObject = aClass.newInstance();
+                    Method invokeMethod = findInvokeMethod(aObject);
+                    Object response = invokeMethod.invoke(aObject, object);
+                    String agentHeader = Base64.decode2UTFString(response.toString());
+                    if (StringUtil.isNotEmpty(agentHeader)) {
+                        String[] traceContextArray = agentHeader.substring(1, agentHeader.length() - 1).split(",");
+                        for (String traceContext : traceContextArray) {
+                            String[] entry = traceContext.split("=", 2);
+                            agentHeaderMap.put(entry[0].trim(), entry[1].trim());
+                        }
+                        hasAgentHeader = true;
+                    }
+                }
+            } catch (Exception e) {
+                LOGGER.info("union-paygate processing reflection method error:. Exception:{}", e);
+            }
+        }
+
         ContextCarrier contextCarrier = new ContextCarrier();
+        CarrierItem next = contextCarrier.items();
+        while (next.hasNext()) {
+            next = next.next();
+            if (hasAgentHeader) {
+                next.setHeadValue(agentHeaderMap.get(next.getHeadKey()));
+            } else {
+                next.setHeadValue(request.getHeader(next.getHeadKey()));
+            }
+        }
         AbstractSpan span = ContextManager.createEntrySpan(request.getRequestURI(), contextCarrier);
         Tags.URL.set(span, request.getRequestURL().toString());
         HTTP.METHOD.set(span, request.getMethod());
         span.setComponent(ComponentsDefine.UNION_PAYGATE);
         SpanLayer.asHttp(span);
-        HTTP.PARAMS.set(span, dataMap.toString());
     }
 
     @Override
@@ -69,5 +108,20 @@ public class CoreControllerInterceptor implements InstanceMethodsAroundIntercept
     public void handleMethodException(EnhancedInstance objInst, Method method, Object[] allArguments,
                                       Class<?>[] argumentsTypes, Throwable t) {
         ContextManager.activeSpan().log(t);
+    }
+
+    public Method findInvokeMethod(Object object) {
+        Method invokeMethod = null;
+        Method[] methods = object.getClass().getDeclaredMethods();
+        if (methods != null && methods.length > 0) {
+            for (Method m : methods) {
+                Class<?>[] clazzs = m.getParameterTypes();
+                if (m.getName().equals(Config.Agent.ESB_TRACE_CLASS_READ_METHOD) && clazzs.length == 1) {
+                    invokeMethod = m;
+                    break;
+                }
+            }
+        }
+        return invokeMethod;
     }
 }
